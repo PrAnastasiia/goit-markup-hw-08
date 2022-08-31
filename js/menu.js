1358b84c4f3f8942e0e9a8f019c0960afaf29538
PrAnastasiia <nastya.prigarovskaya@gmail.com>
2022-08-31 18:20:28 +0200

    hw8

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-btn]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const page = document.querySelector('[data-menu-page]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('mobile-menu-btn--is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('mobile-backdrop--is-open');
+    page.classList.toggle('page--menu-open');
+  });
+})();
